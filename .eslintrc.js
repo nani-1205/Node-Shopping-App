3f3cdc4e2b1ef62e2bb85014747b627c4df092f8
YETUKURI SAI JAGAN <nani.y9966@gmail.com>
2025-03-06 18:31:51 +0530

    Clean up dependencies and ESLint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,29 +1,12 @@
-// .eslintrc.js
-module.exports = {
-    env: {
-      node: true,
-      es2021: true,
+{
+    "env": {
+      "browser": true,
+      "node": true
     },
-    extends: [
-      'eslint:recommended',
-    ],
-    parserOptions: {
-      ecmaVersion: 'latest',
-      sourceType: 'module',
-    },
-    rules: {
-      'no-unused-vars': 'warn',
-      'no-console': 'off',
-      'indent': ['error', 2],
-      'quotes': ['error', 'single'],
-      'semi': ['error', 'always'],
-    },
-    overrides: [
-      {
-        files: ['.eslintrc.js'],
-        parserOptions: {
-          sourceType: 'script',
-        },
-      },
-    ],
-  };
+    "extends": "eslint:recommended",
+    "rules": {
+      "no-unused-vars": "warn",
+      "no-undef": "off"
+    }
+  }
+  
