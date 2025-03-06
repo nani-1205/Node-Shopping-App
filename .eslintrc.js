05def13f0b82bccca7e5ce37dc9a46b63a16f01a
YETUKURI SAI JAGAN <nani.y9966@gmail.com>
2025-03-06 17:46:37 +0530

    Clean up dependencies and ESLint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,21 @@
 // .eslintrc.js
 module.exports = {
     env: {
-      node: true,     // Specifies that this is a Node.js environment.
-      es2021: true,   // Enables ES2021 (ES12) features.  Use 'es2022', 'es2023', etc. for later versions.
+      node: true,
+      es2021: true,
     },
     extends: [
-      'eslint:recommended', // Includes ESLint's recommended rules for JavaScript.
+      'eslint:recommended',
     ],
     parserOptions: {
-      ecmaVersion: 'latest', // Use the latest ECMAScript version supported by ESLint.
-      sourceType: 'module',  // Use 'module' if you're using ES modules (import/export).  Use 'script' for older CommonJS (require).
+      ecmaVersion: 'latest',
+      sourceType: 'module',
     },
     rules: {
-      // Add your custom rule overrides here.
-      'no-unused-vars': 'warn',  // Warn about unused variables (instead of error).
-      'no-console': 'off',       // Allow console.log statements (good for development, but consider removing in production).
-      'indent': ['error', 2],   // Enforce 2-space indentation.
-      'quotes': ['error', 'single'], // Enforce single quotes.
-      'semi': ['error', 'always'],    // Require semicolons.
+      'no-unused-vars': 'warn',
+      'no-console': 'off',
+      'indent': ['error', 2],
+      'quotes': ['error', 'single'],
+      'semi': ['error', 'always'],
     },
   };
